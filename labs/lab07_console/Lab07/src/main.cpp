ba6500a8143359f7435c5c5416e3f7cd8b10a552
Joziazf <139672450+Joziazf@users.noreply.github.com>
2026-02-10 19:57:46 +0700

    first task(finish) + ignore cmake files

--- labs/lab07_console/Lab07/src/main.cpp
+++ labs/lab07_console/Lab07/src/main.cpp
@@ -3,10 +3,10 @@
 
 int main() {
     // Задание 1: начальная точка
-    double x0 = 0.0;
+    double x0 = 0;
 
     double x_end = 4.0;
-    double step = (x_end - x0) / 9
+    double step = (x_end - x0) / 9;
 
     for (int i = 1; i < 10; i++) {
         double x = x0 + i * step;
@@ -14,7 +14,6 @@ int main() {
         std::cout << fx;
         if (i < 9) std::cout << " ";
     }
-
 
 
 }
