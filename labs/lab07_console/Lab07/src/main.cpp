fe3bde2131392f48e9915537592a2908d0e91411
Joziazf <139672450+Joziazf@users.noreply.github.com>
2026-02-10 20:43:22 +0700

    second task

--- labs/lab07_console/Lab07/src/main.cpp
+++ labs/lab07_console/Lab07/src/main.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <cmath>
+#include <cstdio>
 
 int main() {
     // Задание 1: начальная точка
     double x0 = 0;
 
     double x_end = 4.0;
-    double step = (x_end - x0) / 9;
+    double step = (x_end - x0) / 10.0;
 
-    for (int i = 1; i < 10; i++) {
+    for (int i = 1; i <= 10; i++) {
         double x = x0 + i * step;
         double fx = sin(x) / x;
         std::cout << fx;
-        if (i < 9) std::cout << " ";
+        if (i < 10) std::cout << " ";
     }
+    std::cout << "\n";
 
+    // Задание 2: значение MAX
+    long long MAX = 1000;
 
+    long long fib1 = 1, fib2 = 1;
+    long long S = 0;
+    int N = 0;
+
+    while (S <= MAX) {
+        S += fib1;
+        N++;
+
+        long long next = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = next;
+    }
+    std::cout << N << " " << S;
+    std::cout << "\n";
 }
