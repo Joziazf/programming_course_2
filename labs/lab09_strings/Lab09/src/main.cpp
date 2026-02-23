0fecbd2bf151eb4c0797d2beaaa0ddc82f0607da
Joziazf <139672450+Joziazf@users.noreply.github.com>
2026-02-23 19:28:21 +0700

    fourth task char method

--- labs/lab09_strings/Lab09/src/main.cpp
+++ labs/lab09_strings/Lab09/src/main.cpp
@@ -11,7 +11,7 @@ void first() {
 
     cout << "1. len: " << strlen(c_str_1) << endl; // 1
     cout << "2. append: " << strcat(c_str_1, c_str_2) << endl; // 2
-    cout << "3. Copy: " << strcpy(c_str_copy, c_str_1) << endl; // 3
+    cout << "3. сopy: " << strcpy(c_str_copy, c_str_1) << endl; // 3
 
     // string cpp
     string cpp_str = "School";
@@ -23,9 +23,44 @@ void first() {
 
 }
 
+void fourth_char() {
+    int n = 0;
+
+    // проверка на ввод
+    do {
+        cin >> n;
+        if (n < 1 || n > 20) {
+            cout << "Invalid input. n -> [1; 20]" << endl;
+        }
+    } while (n < 1 || n > 20); // для проверки n, чтобы if-ом не прерывать выполнение всей программы
+
+    char words[20][11]; // количество слов - 20; количество символов - 11, тк есть символ окончания строки "\0"
+    char temp[100]; // просто буфер
+
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+
+        // 1 вариант
+        if (strlen(temp) > 10) {
+            temp[10] = '\n'; // просто обрежу строку (11 символ = '\n')
+        }
+
+        // 2 вариант (не нравится мне)
+        // if (strlen(temp) > 10) {
+        //     i--;
+        //     cout << "len > 10. Try again" << endl;
+        // }
+
+        strcpy(words[i], temp);
+    }
+    for (int i = 1; i < n; i += 2) {
+        cout << words[i] << endl;
+    }
+    cout << endl;
+}
+
 int main() {
     first();
+    fourth_char();
     return 0;
 }
-//упражнение 4 -> реализовать через char и через str, ввод через клаву значений до 20, проверка из условия
-//реализация через войды и основной main
