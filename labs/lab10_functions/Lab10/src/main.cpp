80e91d775c41b02867536b975c7b0a6a1cc0c317
Joziazf <139672450+Joziazf@users.noreply.github.com>
2026-03-02 17:19:18 +0700

    second full

--- labs/lab10_functions/Lab10/src/main.cpp
+++ labs/lab10_functions/Lab10/src/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 // arr - исходный массив, n - длина исходника, mpack - запакованный массив.
 void pack(int* arr, int n, int* mpack) {
-    for (int i = 0; i < 17; i++) {
+    for (int i = 0; i <= 17; i++) {
         mpack[i] = 0;
     }
 
@@ -25,6 +25,33 @@ void raspack(int* mpack, int* arr) {
             cur++;
         }
     }
+}
 
+int main() {
+    const int n = 10000;
 
+    int* orig_arr = new int[n]; // выделение памяти под массив
+
+    for (int i = 0; i < n; i++) {
+        orig_arr[i] = (i * 18) / n;
+    }
+
+    int pack_arr[18];
+
+    pack(orig_arr, n, pack_arr);
+
+    for (int i = 0; i <= 17; i++) {
+        cout << i << ": " << pack_arr[i] << endl;
+    }
+
+    int* repack = new int[n];
+
+    raspack(pack_arr, repack);
+
+    for (int i = 0; i < n; i++) {
+        cout << repack[i] << endl;
+    }
+
+    delete[] orig_arr;
+    delete[] repack;
 }
