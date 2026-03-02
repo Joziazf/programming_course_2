0c38e49ec206f1d84269f88a5a97482c2bb1c0cf
Joziazf <139672450+Joziazf@users.noreply.github.com>
2026-03-02 16:43:46 +0700

    explain first ex + funcs second ex

--- labs/lab10_functions/Lab10/src/main.cpp
+++ labs/lab10_functions/Lab10/src/main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+using namespace std;
 
-// Lab 10
-// TODO: реализуйте решение по заданию в labs/lab10_functions/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
-int main() {
-    // TODO
-    return 0;
+
+// arr - исходный массив, n - длина исходника, mpack - запакованный массив.
+void pack(int* arr, int n, int* mpack) {
+    for (int i = 0; i < 17; i++) {
+        mpack[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int val = arr[i];
+        mpack[val]++;
+    }
+}
+
+void raspack(int* mpack, int* arr) {
+    int cur = 0;
+
+    for (int i = 0; i <= 17; i++) {
+        int k = mpack[i]; // высчитываем кол-во этого элемента в исходном массиве
+
+        for (int j = 0; j < k; j++) { // записываем этот элемент k раз
+            arr[cur] = i;
+            cur++;
+        }
+    }
+
+
 }
